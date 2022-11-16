2778bf3e1cdeaa27ad7a51c75698b8494c6cd48b
rpcmoreira <82443429+rpcmoreira@users.noreply.github.com>
2022-11-16 17:20:21 +0000

    Master (#2)
    * commit
    
    * correcao sonarcloud
    
    * wait
    
    * more fix

--- src/main/java/examples/Example2.java
+++ src/main/java/examples/Example2.java
@@ -4,18 +4,10 @@ public class Example2 {
 
     public static void main(String[] args) {
         sleepInSynchronized();
-        emptySynchronized();
     }
 
     private void emptySynchronized() {
         synchronized (this) {
             exit(-1);
-        }
-    }
-
-    private void sleepInSynchronized() throws InterruptedException {
-        synchronized (this) {
-           wait();
-        }
     }
 }
